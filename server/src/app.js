bb3b218e582be5887518abe478822fa0574eecf1
Faraz <farazatgithub@gmail.com>
2023-02-21 15:06:31 +0500

    loggin requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const path = require("path")
+const morgan = require("morgan")
 
 const planetsRouter = require("./routes/planets.router")
 
@@ -11,8 +12,20 @@ app.use(cors({
     origin:"http://localhost:3000"
 }))
 
+//* custom middleware for logging request
+// app.use((req,res,next) => {
+//     next()
+//     console.log(`${req.method}${req.baseUrl}${req.url}`)
+// })
+
+//* morgan middlware for logging request information coming in
+app.use(morgan("combined"))
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname,"..","public")))
+
+
+
 app.use("/planets",planetsRouter )
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,"..","public","index.html"))
