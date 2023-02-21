db99037363423ac6321b138754ab960f80c779e1
Faraz <farazatgithub@gmail.com>
2023-02-21 14:18:36 +0500

    initial commit

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,30 @@
+const URL = "http://localhost:8000/planets"
+
+async function httpGetPlanets() {
+  const resp = await fetch(URL)
+  return await resp.json()
+  // TODO: Once API is ready.
+  // Load planets and return as JSON.
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
