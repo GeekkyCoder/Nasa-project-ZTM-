d97b21dbb56b8d0b9e38743e519e2b480b24c2ee
Faraz <farazatgithub@gmail.com>
2023-02-21 22:13:48 +0500

    fixed navigation urls with express and react

--- server/src/app.js
+++ server/src/app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname,"..","public")))
 
 app.use("/planets",planetsRouter )
 app.use("/launches",getAllLaunches)
-app.get("/",(req,res) => {
+app.get("/*",(req,res) => {
     res.sendFile(path.join(__dirname,"..","public","index.html"))
 })
 
