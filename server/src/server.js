54c1d14efbbc052d801f96bd9d6a1400f5613e05
Faraz <farazatgithub@gmail.com>
2023-02-25 18:52:16 +0500

    added pm2 package for performance

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,8 @@ const http = require("http")
 const app = require("./app")
 const {loadPlanetsData} = require("./modals/planets.modal")
 
+const MONGO_URL = "mongodb+srv://nasa-api:PnzsSHxFSmV6E02i@nasacluster.ztif9db.mongodb.net/?retryWrites=true&w=majority"
+
 const PORT = process.env.PORT || 8000
 
 const server = http.createServer(app)
